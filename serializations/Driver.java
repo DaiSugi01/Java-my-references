2c5a56b0b9e7f5354e3fe7b297f3520865491073
Daisugi <volble124@gmail.com>
2020-11-14 23:39:17 +0900

    add serializations

--- serializations/Driver.java
+++ serializations/Driver.java
@@ -13,14 +13,27 @@ public class Driver {
         System.out.println(d);
 
         try {
+
+            /**
+             * Output Object
+             */
             FileOutputStream fos = new FileOutputStream("dg.ser");
+            // if there is no dg.ser, FileOutputStream create new file.
             ObjectOutputStream oos = new ObjectOutputStream(fos);
+            // if this cannot find dg.ser, ObjectOutputStream create new file.
             oos.writeObject(d);
             oos.close();
+
+            /**
+             * Input Object
+             * */
+            // if this cannot find dg.ser, throws error
             FileInputStream fis = new FileInputStream("dg.ser");
             ObjectInputStream ois = new ObjectInputStream(fis);
+            // readObject() returns Object type object. so you need to cast it to particular object.
             d = (DungeonGame) ois.readObject();
             ois.close();
+
         } catch (Exception ex) {
             ex.printStackTrace();
         }
